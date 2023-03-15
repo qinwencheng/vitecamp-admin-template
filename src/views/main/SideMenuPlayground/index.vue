ab978b509a6ace051f753d58982df64474037a03
qinwencheng <157475034@qq.com>
2023-03-15 21:11:40 +0800

    fix: 解决滚动条问题(#1)

--- src/views/main/SideMenuPlayground/index.vue
+++ src/views/main/SideMenuPlayground/index.vue
@@ -86,7 +86,7 @@ const handleClose = (key: string, keyPath: string[]) => {
 .side-menu .el-sub-menu__title {
   border-radius: 3px;
   height: 42px;
-  margin-top: 6px;
+  margin-bottom: 6px;
 }
 
 /* 默认情况下菜单项的hover的样式 */
