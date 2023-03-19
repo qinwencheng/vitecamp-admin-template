5e12a3039da4325930d23cbea345c0b06da1830d
qinwencheng <157475034@qq.com>
2023-03-19 23:59:13 +0800

    feat(components): :sparkles: 新增侧边栏菜单组件
    - 使用tsx的写法完成相关的递归操作
    - 在index.vue中少拆了一个用于循环展开菜单项的组件
    - 目的是为了能利用上同一个props，少传一次props
    - 后期看情况可能会拆开

---
+++ src/components/SideMenu/index.vue
@@ -0,0 +1,79 @@
+<script setup lang='tsx'>
+import type { RouteRecordRaw } from 'vue-router'
+import MenuItem from '~/components/SideMenu/MenuItem'
+
+const sideMenuProps = defineProps({
+  itemList: {
+    type: Object as PropType<RouteRecordRaw[]>,
+    required: true,
+  },
+})
+
+// 为了少定义一次props
+const MenuContent = defineComponent({
+  setup() {
+    return () => (
+      <>
+        {
+          /* 使用外部组件的props */
+          sideMenuProps.itemList?.map(x => (<MenuItem item={x} />))
+        }
+      </>
+    )
+  },
+})
+</script>
+
+<template>
+  <el-menu id="side-menu" default-active="2" class="h-full" unique-opened router>
+    <MenuContent :item-list="sideMenuProps.itemList" />
+  </el-menu>
+</template>
+
+<style lang="postcss">
+/* 默认情况下菜单项的样式 */
+#side-menu .el-menu-item,
+#side-menu .el-sub-menu__title {
+  border-radius: 3px;
+  height: 42px;
+  margin-top: 6px;
+}
+
+/* 默认情况下菜单项的hover的样式 */
+#side-menu .el-menu-item::before,
+#side-menu .el-sub-menu__title::before {
+  left: 5px;
+  right: 5px;
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  pointer-events: none;
+  border-radius: 3px;
+  transition: background-color .3s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+/* 当鼠标放到某个菜单项上时的左边竖线 */
+#side-menu .el-menu-item:hover::before,
+#side-menu .el-sub-menu__title:hover::before,
+#side-menu .el-menu-item.is-active::before {
+  border-left: 4px solid var(--el-color-primary);
+}
+
+/* 当鼠标放到某个菜单项上时的样式 */
+#side-menu .el-menu-item:hover,
+#side-menu .el-sub-menu__title:hover {
+  background-color: #f3f3f5;
+}
+
+/* 某个被选中激活的菜单项的样式 */
+#side-menu .el-menu-item.is-active {
+  background-color: var(--el-color-primary-light-9);
+}
+
+/* 二级菜单的伸缩动画 */
+#side-menu .el-collapse-transition-enter-active,
+#side-menu .el-collapse-transition-leave-active {
+  transition: max-height .2s cubic-bezier(.4, 0, .2, 1) 0s;
+}
+</style>
