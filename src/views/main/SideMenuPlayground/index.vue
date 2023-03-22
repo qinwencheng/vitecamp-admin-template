66c0ba83b8f5cfa7f9a357a95235a591205c6362
qinwencheng <157475034@qq.com>
2023-03-22 21:49:37 +0800

    feat: :sparkles:调整伸缩动画的速度

--- src/views/main/SideMenuPlayground/index.vue
+++ src/views/main/SideMenuPlayground/index.vue
@@ -18,7 +18,7 @@ const handleClose = (key: string, keyPath: string[]) => {
     <transition name="fade-slide" mode="out-in" appear>
       <el-container class="h-full w-full">
         <el-aside width="200px">
-          <el-menu default-active="2" class="side-menu h-full" @open="handleOpen" @close="handleClose">
+          <el-menu id="side-menu" default-active="2" class="h-full" @open="handleOpen" @close="handleClose">
             <el-sub-menu index="1">
               <template #title>
                 <el-icon>
@@ -82,16 +82,16 @@ const handleClose = (key: string, keyPath: string[]) => {
 
 <style lang="postcss">
 /* 默认情况下菜单项的样式 */
-.side-menu .el-menu-item,
-.side-menu .el-sub-menu__title {
+#side-menu .el-menu-item,
+#side-menu .el-sub-menu__title {
   border-radius: 3px;
   height: 42px;
   margin-bottom: 6px;
 }
 
 /* 默认情况下菜单项的hover的样式 */
-.side-menu .el-menu-item::before,
-.side-menu .el-sub-menu__title::before {
+#side-menu .el-menu-item::before,
+#side-menu .el-sub-menu__title::before {
   left: 5px;
   right: 5px;
   content: "";
@@ -104,20 +104,26 @@ const handleClose = (key: string, keyPath: string[]) => {
 }
 
 /* 当鼠标放到某个菜单项上时的左边竖线 */
-.side-menu .el-menu-item:hover::before,
-.side-menu .el-sub-menu__title:hover::before,
-.side-menu .el-menu-item.is-active::before {
+#side-menu .el-menu-item:hover::before,
+#side-menu .el-sub-menu__title:hover::before,
+#side-menu .el-menu-item.is-active::before {
   border-left: 4px solid var(--el-color-primary);
 }
 
 /* 当鼠标放到某个菜单项上时的样式 */
-.side-menu .el-menu-item:hover,
-.side-menu .el-sub-menu__title:hover {
+#side-menu .el-menu-item:hover,
+#side-menu .el-sub-menu__title:hover {
   background-color: #f3f3f5;
 }
 
 /* 某个被选中激活的菜单项的样式 */
-.side-menu .el-menu-item.is-active {
+#side-menu .el-menu-item.is-active {
   background-color: var(--el-color-primary-light-9);
+}
+
+/* 二级菜单的伸缩动画 */
+#side-menu .el-collapse-transition-enter-active,
+#side-menu .el-collapse-transition-leave-active {
+  transition: max-height .2s cubic-bezier(.4, 0, .2, 1) 0s;
 }
 </style>
