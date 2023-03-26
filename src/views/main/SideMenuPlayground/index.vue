c95ab77cfc96c2cfd2b5229efa1fb8d58e6c7394
qinwencheng <157475034@qq.com>
2023-03-26 18:57:20 +0800

    feat(components): :construction: 初步引入logo组件到侧边栏组件中

--- src/views/main/SideMenuPlayground/index.vue
+++ src/views/main/SideMenuPlayground/index.vue
@@ -1,6 +1,12 @@
 <script lang="ts" setup>
+import { storeToRefs } from 'pinia'
 import { mainRouter } from '~/router/modules/main/index'
 import SideMenu from '~/components/SideMenu/index.vue'
+import { useAppStore } from '~/store/modules'
+
+const appStore = useAppStore()
+const { isSideMenuCollapsed } = storeToRefs(appStore)
+const { toggleSideMenuCollapse } = appStore
 
 const handleOpen = (key: string, keyPath: string[]) => {
   // console.log(key, keyPath)
@@ -8,22 +14,22 @@ const handleOpen = (key: string, keyPath: string[]) => {
 const handleClose = (key: string, keyPath: string[]) => {
   // console.log(key, keyPath)
 }
-const isCollapse = ref(true)
+
 const sideWidth = computed(() => {
-  return isCollapse.value ? 'w-64px' : 'w-200px'
+  return isSideMenuCollapsed.value ? 'w-64px' : 'w-220px'
 })
-const open = useToggle(isCollapse)
 </script>
 
 <template>
-  <el-button @click="open()">
+  <el-button @click="toggleSideMenuCollapse()">
     open
   </el-button>
   <div class="side-menu-playground-page-container h-[calc(100vh-30px)] w-screen bg-[#f5f6fb]">
     <transition name="fade-slide" mode="out-in" appear>
       <el-container class="h-full w-full">
-        <div>
-          <SideMenu :item-list="mainRouter" :collapse="isCollapse" :class="sideWidth" />
+        <div :class="sideWidth">
+          <SideLogo />
+          <SideMenu :item-list="mainRouter" :collapse="isSideMenuCollapsed" />
         </div>
         <el-main>
           <section class="h-full w-full bg-[#f5f6fb]">
