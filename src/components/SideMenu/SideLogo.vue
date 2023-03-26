4dded93e2c22fdf1479317a8bd51efa76d341b69
qinwencheng <157475034@qq.com>
2023-03-26 18:05:37 +0800

    feat(components): :sparkles: 新增侧边栏的Logo组件

---
+++ src/components/SideMenu/SideLogo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useAppStore } from '~/store/modules/index'
+
+const appStore = useAppStore()
+const title = import.meta.env.VITE_APP_TITLE
+</script>
+
+<template>
+  <div class="bg-#fff w-full">
+    <router-link class="h-15 flex items-center justify-center" to="/">
+      <div class="i-tabler-layout-dashboard text-9 text-[var(--el-color-primary)]" />
+      <h2 v-show="!appStore.isSideMenuCollapsed" class="ml-2.5 text-[var(--el-color-primary)] text-6 font-bold max-w-35 flex-shrink-0">
+        {{ title }}
+      </h2>
+    </router-link>
+  </div>
+</template>
