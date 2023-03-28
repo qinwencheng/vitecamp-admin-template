0ffe55e2c2020b93d7b145ce4c82ab84199bd571
qinwencheng <157475034@qq.com>
2023-03-28 23:59:57 +0800

    feat(components): :construction: 新增顶部栏组件
    - 完成折叠小图标

---
+++ src/components/HeaderBar/index.vue
@@ -0,0 +1,18 @@
+import { useAppStore } from '~/store/modules';
+
+<script setup lang="ts">
+import { useAppStore } from '~/store/modules'
+
+const appStore = useAppStore()
+</script>
+
+<template>
+  <div class="header-bar bg-white w-full h-full border-b border-[#eee] flex px-15px">
+    <div class="left-tool flex items-center">
+      <el-icon size="20" @click="appStore.toggleSideMenuCollapse()">
+        <div v-if="appStore.isSideMenuCollapsed" class="i-mdi-format-indent-increase" />
+        <div v-else class="i-mdi-format-indent-decrease" />
+      </el-icon>
+    </div>
+  </div>
+</template>
