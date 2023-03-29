6b7e9b5aaacd79da4584a0c7b27fd7aafb9e664e
qinwencheng <157475034@qq.com>
2023-03-29 23:10:04 +0800

    feat(components): :sparkles: 新增暗黑模式功能按钮

---
+++ src/components/HeaderBar/DarkMode.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { isDark, toggleDark } from '~/utils/dark'
+</script>
+
+<template>
+  <button class="icon-btn mx-2 !outline-none" @click="toggleDark()">
+    <i-ph-cloud-moon-bold v-if="isDark" />
+    <i-ph-sun-horizon-bold v-else />
+  </button>
+</template>
