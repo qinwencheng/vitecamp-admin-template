05ae9e430524cb99fa42dcb29529ca3dcea625b2
qinwencheng <157475034@qq.com>
2023-03-29 23:11:06 +0800

    feat(components): :sparkles: 新增Github仓库页跳转功能按钮

---
+++ src/components/HeaderBar/GithubSite.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const handleLinkClick = () => {
+  window.open('https://github.com/qinwencheng/vitecamp-admin-template')
+}
+</script>
+
+<template>
+  <button class="icon-btn mx-2 !outline-none" @click="handleLinkClick">
+    <i-mdi-github />
+  </button>
+</template>
