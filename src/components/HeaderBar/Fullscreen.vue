4c5efc29c090d42cc10cd0be6be1001da5de2d28
qinwencheng <157475034@qq.com>
2023-03-31 00:48:11 +0800

    feat(components): :sparkles: 新增全屏功能按钮

---
+++ src/components/HeaderBar/Fullscreen.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core'
+
+const { isFullscreen, toggle } = useFullscreen()
+</script>
+
+<template>
+  <button class="icon-btn mx-2 !outline-none" @click="toggle()">
+    <i-ant-design-fullscreen-exit-outlined v-if="isFullscreen" />
+    <i-ant-design-fullscreen-outlined v-else />
+  </button>
+</template>
