c02e5776d5c2122018b7c0c3bd10453209ac9929
qinwencheng <157475034@qq.com>
2023-04-03 22:26:09 +0800

    feat(components): :lipstick: 重写el-breadcrumb的一些默认样式
    - 让最后的元素（表示当前页面）字体默认亮黑，鼠标效果为普通
    - 非最后的元素字体变灰
    - 非最后的元素有鼠标的pointer效果
    - 非最后的元素在hover时有灰色背景方框

--- src/components/HeaderBar/Breadcrumb.vue
+++ src/components/HeaderBar/Breadcrumb.vue
@@ -9,7 +9,7 @@ function handleBreadClick(path: string) {
 </script>
 
 <template>
-  <el-breadcrumb separator="/">
+  <el-breadcrumb id="breadcrumb" separator="/">
     <el-breadcrumb-item
       v-for="item in route.matched.filter(item => !!item.meta?.title && item.meta?.title !== 'main')"
       :key="item.path"
@@ -24,3 +24,41 @@ function handleBreadClick(path: string) {
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
+
+<style lang="scss">
+#breadcrumb {
+
+  // 重写el-breadcrumb的默认样式
+  .el-breadcrumb__item .el-breadcrumb__inner,
+  .el-breadcrumb__item .el-breadcrumb__inner a {
+    padding: 4px;
+    border-radius: 3px;
+    transition: background-color .3s cubic-bezier(0.4, 0, 0.2, 1), color .3s cubic-bezier(0.4, 0, 0.2, 1);
+    position: relative;
+  }
+
+  // 非最后一个元素的字体变灰，且有鼠标和hover效果
+  .el-breadcrumb__item:not(:last-child) .el-breadcrumb__inner,
+  .el-breadcrumb__item:not(:last-child) .el-breadcrumb__inner a {
+    font-weight: normal;
+    color: var(--el-text-color-secondary);
+    cursor: pointer;
+
+    // hover时字体亮黑，且有灰色方框背景
+    &:hover {
+      background-color: rgba(46, 51, 56, 0.09);
+      color: var(--el-text-color-primary);
+    }
+  }
+
+  // 最后一个元素(表示当前页面)的字体默认亮黑，且没有鼠标和hover效果
+  .el-breadcrumb__item:last-child .el-breadcrumb__inner,
+  .el-breadcrumb__item:last-child .el-breadcrumb__inner:hover,
+  .el-breadcrumb__item:last-child .el-breadcrumb__inner a,
+  .el-breadcrumb__item:last-child .el-breadcrumb__inner a:hover {
+    font-weight: normal;
+    color: var(--el-text-color-primary);
+    cursor: unset;
+  }
+}
+</style>
