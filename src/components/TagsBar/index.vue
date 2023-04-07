5477c38c34df7c9749a302a305cec5340e4a482c
qinwencheng <157475034@qq.com>
2023-04-07 20:29:31 +0800

    feat(components): :construction: 新增tagsbar组件
    - 待完善

---
+++ src/components/TagsBar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="[&_.el-tag]:( h-7 mx-10px text-size-[var(--el-font-size-base)])">
+    <el-tag class="inline-block" closable>
+      工作台
+    </el-tag>
+    <el-tag class="inline-block" closable>
+      工作台
+    </el-tag>
+    <el-tag class="inline-block" closable>
+      工作台
+    </el-tag>
+  </div>
+</template>
