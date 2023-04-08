9e991c8f4a5781e8daf142889cf0e369aa88a413
qinwencheng <157475034@qq.com>
2023-04-08 22:45:39 +0800

    feat(components): :sparkles: 新增tags的点击删除标签的功能

--- src/store/modules/tags/index.ts
+++ src/store/modules/tags/index.ts
@@ -7,14 +7,24 @@ interface Tag {
 
 export const useTagsStore = defineStore('tags', () => {
   const tagsList = ref<Tag[]>([])
+
   const addTag = (tag: Tag) => {
     if (tagsList.value.some(x => x.path === tag.path))
       return
 
     tagsList.value.push(tag)
   }
+
+  const deleteTag = (path: string) => {
+    if (tagsList.value.length === 1)
+      return
+    const index = tagsList.value.findIndex(x => x.path === path)
+    tagsList.value.splice(index, 1)
+  }
+
   return {
     tagsList,
     addTag,
+    deleteTag,
   }
 })
