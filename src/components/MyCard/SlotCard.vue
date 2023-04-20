91a80b943d6b218cf16ddde296b7f250367b1715
qinwencheng <157475034@qq.com>
2023-04-20 23:35:13 +0800

    feat(components): :lipstick: 增加SlotCard对暗黑模式的支持

--- src/components/MyCard/SlotCard.vue
+++ src/components/MyCard/SlotCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="my-card box-border rounded-3px bg-#fff leading-relaxed break-words text-14px children:(px-4 py-3)">
-    <div v-if="$slots['header-main'] || $slots['header-extra']" class="header text-4 flex items-center justify-between b-b">
+  <div class="my-card box-border rounded-3px bg-#fff dark:bg-#18181c leading-relaxed break-words text-14px children:(px-4 py-3)">
+    <div v-if="$slots['header-main'] || $slots['header-extra']" class="header text-4 flex items-center justify-between b-b border-[var(--el-border-color)]">
       <div v-if="$slots['header-main']" class="card-header__main w-full" role="heading">
         <slot name="header-main" />
       </div>
@@ -13,7 +13,7 @@
       <slot />
     </div>
 
-    <div v-if="$slots.footer" class="b-t">
+    <div v-if="$slots.footer" class="b-t border-[var(--el-border-color)]">
       <slot class="footer" name="footer" />
     </div>
   </div>
