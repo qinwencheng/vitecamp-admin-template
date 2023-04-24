c76cfa29b8476d9421fe872fcb6d2d8d9bbb313c
qinwencheng <157475034@qq.com>
2023-04-24 22:56:47 +0800

    feat(components): :lipstick: 新增hover时字体变色功能(#9)

--- src/components/TagsBar/RouteTag.vue
+++ src/components/TagsBar/RouteTag.vue
@@ -9,7 +9,7 @@ const isActiveTag = computed(() => props.path === route.path)
 </script>
 
 <template>
-  <el-tag :type="isActiveTag ? '' : 'info'" class="[&.el-tag]:(h-7 mx-10px text-size-[var(--el-font-size-base)] cursor-pointer)">
+  <el-tag :type="isActiveTag ? '' : 'info'" class="[&.el-tag]:(h-7 mx-10px text-size-[var(--el-font-size-base)] cursor-pointer)" :class="{ 'hover:text-[var(--el-color-primary)]': !isActiveTag }">
     {{ props.title }}
   </el-tag>
 </template>
