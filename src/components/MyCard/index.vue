91563ae9ed41780fde8ad7e8dde098726d83a6de
qinwencheng <157475034@qq.com>
2023-05-02 00:18:00 +0800

    feat(components): :sparkles: 给卡片新增title属性选项(#10)

--- src/components/MyCard/index.vue
+++ src/components/MyCard/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps<{
   bordered?: boolean
+  title?: string
 }>()
 </script>
 
@@ -9,10 +10,15 @@ const props = defineProps<{
     class="my-card box-border rounded-3px bg-#fff dark:bg-#18181c leading-relaxed break-words text-14px children:(px-4 py-3)"
     :class="{ 'b-1px b-solid b-[var(--border-color)]': props.bordered }"
   >
-    <div v-if="$slots['header-main'] || $slots['header-extra']" class="header text-4 flex items-center justify-between b-b border-[var(--el-border-color)]">
+    <div v-if="props.title || $slots['header-main'] || $slots['header-extra']" class="header text-4 flex items-center justify-between b-b border-[var(--el-border-color)]">
+      <!-- header-main or props.title -->
       <div v-if="$slots['header-main']" class="card-header__main w-full" role="heading">
         <slot name="header-main" />
       </div>
+      <div v-else-if="props.title" class="card-header__main w-full" role="heading">
+        {{ props.title }}
+      </div>
+
       <div v-if="$slots['header-extra']" class="card-header__extra">
         <slot name="header-extra" />
       </div>
