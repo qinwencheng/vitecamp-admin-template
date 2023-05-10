b58ab1601ab1a6108cd7de128f6af410b26a6fb6
qinwencheng <157475034@qq.com>
2023-05-11 00:21:09 +0800

    feat(components): :lipstick: 统一卡片的边框颜色
    - 发现当前卡片的边框颜色在亮色模式下过于突出
    - 把header和footer的边框颜色从el变量改为自定义的变量

--- src/components/MyCard/index.vue
+++ src/components/MyCard/index.vue
@@ -12,13 +12,13 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <div
-    class="my-card box-border rounded-3px bg-#fff dark:bg-#18181c leading-relaxed break-words text-14px children:(px-4 py-3)"
+    class="my-card box-border rounded-3px bg-#fff dark:bg-#18181c leading-[1.6] break-words text-14px children:(px-4 py-3)"
     :class="{ 'b-1px b-solid b-[var(--border-color)]': props.bordered, 'hover:shadow-[var(--box-shadow)]': props.shadow === 'hover', 'shadow-[var(--box-shadow)]': ['always', true].includes(props.shadow) }"
   >
     <div
       v-if="props.title || $slots.header || $slots['header-extra']"
       class="header text-4 flex items-center justify-between"
-      :class="{ 'b-b border-[var(--el-border-color)]': props.bordered }"
+      :class="{ 'b-b border-[var(--border-color)]': props.bordered }"
     >
       <!-- header or props.title -->
       <div v-if="$slots.header" class="card-header__main w-full" role="heading">
@@ -37,7 +37,7 @@ const props = withDefaults(defineProps<Props>(), {
       <slot />
     </div>
 
-    <div v-if="$slots.footer" :class="{ 'b-t border-[var(--el-border-color)]': props.bordered }">
+    <div v-if="$slots.footer" :class="{ 'b-t border-[var(--border-color)]': props.bordered }">
       <slot class="footer" name="footer" />
     </div>
   </div>
