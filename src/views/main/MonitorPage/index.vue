b914dfd0a0c91b4f5d7cdc5f40b1dae59605f7a1
qinwencheng <157475034@qq.com>
2023-05-13 00:23:59 +0800

    feat(components): :sparkles: 新增数据卡片展示

--- src/views/main/MonitorPage/index.vue
+++ src/views/main/MonitorPage/index.vue
@@ -1,3 +1,177 @@
 <template>
   <span>MonitorPage</span>
+  <div class="card-container" grid="~ gap-x-3 gap-y-2 cols-3">
+    <MyCard bordered>
+      <template #header>
+        <div class="card-header__main" role="heading">
+          访问量
+        </div>
+      </template>
+
+      <template #header-extra>
+        <div class="card-header__extra">
+          <el-tag type="success">
+            日
+          </el-tag>
+        </div>
+      </template>
+
+      <div class="flex justify-between px-1 py-1">
+        <span class="text-3xl">91,942</span>
+      </div>
+
+      <div class="flex justify-between px-1 py-1">
+        <div>
+          日同比
+          <span>54%</span>
+          <div class="i-ant-design-caret-up-filled bg-[var(--el-color-success)] text-12px" />
+        </div>
+        <div>
+          周同比
+          <span>65%</span>
+          <div class="i-ant-design-caret-down-filled bg-[var(--el-color-warning)] text-12px" />
+        </div>
+      </div>
+      <template #footer>
+        <div class="flex justify-between">
+          <div>
+            总访问量：
+          </div>
+          <div>
+            <span>484,844</span>
+          </div>
+        </div>
+      </template>
+    </MyCard>
+
+    <MyCard bordered>
+      <template #header>
+        <div class="card-header__main" role="heading">
+          销售额
+        </div>
+      </template>
+
+      <template #header-extra>
+        <div class="card-header__extra">
+          <el-tag type="warning">
+            周
+          </el-tag>
+        </div>
+      </template>
+
+      <div class="flex justify-between px-1 py-1">
+        <span class="text-3xl">￥68,837</span>
+      </div>
+
+      <div class="px-1 py-1 flex">
+        <div class="flex-1">
+          <el-progress :text-inside="true" :stroke-width="16" :percentage="79" />
+        </div>
+      </div>
+      <template #footer>
+        <div class="flex justify-between">
+          <div>
+            总销售额：
+          </div>
+          <div>
+            <span>￥194,147</span>
+          </div>
+        </div>
+      </template>
+    </MyCard>
+
+    <MyCard bordered>
+      <template #header>
+        <div class="card-header__main" role="heading">
+          订单量
+        </div>
+      </template>
+
+      <template #header-extra>
+        <div class="card-header__extra">
+          <el-tag>周</el-tag>
+        </div>
+      </template>
+
+      <div class="flex justify-between px-1 py-1">
+        <span class="text-3xl">42,519</span>
+      </div>
+
+      <div class="flex justify-between px-1 py-1">
+        <div>
+          日同比
+          <span>66%</span>
+          <div class="i-ant-design-caret-up-filled bg-[var(--el-color-success)] text-12px" />
+        </div>
+        <div>
+          周同比
+          <span>69%</span>
+          <div class="i-ant-design-caret-down-filled bg-[var(--el-color-warning)] text-12px" />
+        </div>
+      </div>
+      <template #footer>
+        <div class="flex justify-between">
+          <div>
+            转化率：
+          </div>
+          <div>
+            <span>196,488%</span>
+          </div>
+        </div>
+      </template>
+    </MyCard>
+
+    <MyCard bordered>
+      <template #header>
+        <div class="card-header__main" role="heading">
+          成交额
+        </div>
+      </template>
+
+      <template #header-extra>
+        <div class="card-header__extra">
+          <el-tag type="danger">
+            周
+          </el-tag>
+        </div>
+      </template>
+
+      <div class="flex justify-between px-1 py-1">
+        <span class="text-3xl">￥15,404</span>
+      </div>
+
+      <div class="flex justify-between px-1 py-1">
+        <div>
+          月同比
+          <span>11%</span>
+          <div class="i-ant-design-caret-up-filled bg-[var(--el-color-success)] text-12px" />
+        </div>
+        <div>
+          周同比
+          <span>22%</span>
+          <div class="i-ant-design-caret-down-filled bg-[var(--el-color-warning)] text-12px" />
+        </div>
+      </div>
+      <template #footer>
+        <div class="flex justify-between">
+          <div>
+            总成交额：
+          </div>
+          <div>
+            <span>￥447,413</span>
+          </div>
+        </div>
+      </template>
+    </MyCard>
+  </div>
 </template>
+
+<!--
+
+  feat(components): :sparkles: 新增数据卡片容器
+
+- 640px起，一行排列两个
+- 1024px起，一行排列三个
+- 1280px起，一行排列四个
+
+ -->
